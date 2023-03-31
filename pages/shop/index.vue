313e82ac4f558aff8393ea141e0835c4954c1318
nichesuch <niche.and.such@gmail.com>
2023-04-01 02:35:52 +0900

    ポイント履歴を合算

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -4,16 +4,29 @@
       店舗
     </template>
     <VCard class="my-4">
-      <VCardTitle>利用ポイント</VCardTitle>
-      <VCardText>9999pt</VCardText>
+      <VCardTitle>利用もりポ</VCardTitle>
+      <VCardText>{{ usedPoint }} もりポ</VCardText>
     </VCard>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
+import { Ticket } from '~~/types/ticket'
 
 definePageMeta({
   middleware: ['auth']
+})
+
+const { token } = useAuth()
+const config = useRuntimeConfig()
+
+const usedPoint = await useFetch<Array<Ticket>>(config.API_ENDPOINT + '/shop/pointticket/history', {
+  headers: {
+    authorization: `${token.value}`
+  },
+  watch: [token]
+}).then((data) => {
+  return data.data.value?.map(a => a.amount).reduce((a, b) => a + b)
 })
 
 </script>
